d807013737022d3d4ba1af11106de15674ce71fb
DaredMett <mettmerdok41@gmail.com>
2024-10-25 20:10:42 +0500

    AnimationController + Cinemachine

--- Assets/Scripts/View.cs
+++ Assets/Scripts/View.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class View : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator MineAnimator;
+    private Rigidbody2D rb;
+    private SpriteRenderer spRender;
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        MineAnimator = GetComponent<Animator>();
+        spRender = GetComponent<SpriteRenderer>();
     }
-
-    // Update is called once per frame
-    void Update()
+    public void WalkAnimatorOn()
     {
-        
+        MineAnimator.SetBool("Walk", true);
+    }
+    public void WalkAnimatorOff()
+    {
+        MineAnimator.SetBool("Walk", false);
+    }
+    public void Update()
+    {
+        if (rb.velocity.magnitude > 0)
+        {
+            WalkAnimatorOn();
+        }
+        else
+        {
+            WalkAnimatorOff();
+        }
+        if (rb.velocity.x < -0.1f)
+        {
+            spRender.flipX = true;
+        }
+        if(rb.velocity.x > 0.1f)
+        {
+            spRender.flipX= false;
+        }
     }
 }
